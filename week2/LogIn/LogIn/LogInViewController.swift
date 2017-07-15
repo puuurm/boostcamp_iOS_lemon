999597fbca09dcc6c15db48e6fddc91133250ba8
yang hee jung <puuurm@naver.com>
2017-07-14 20:23:32 -0700

    add omited LogInViewController

--- week2/LogIn/LogIn/LogInViewController.swift
+++ week2/LogIn/LogIn/LogInViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-    
-    
+    // MARK: Properties
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
     
-    @IBAction func SignIn(_ sender: UIButton) {
+    
+    @IBAction func signIn(_ sender: UIButton) {
         print("touch up inside - sign in")
         print("ID : \(idTextField.text ?? ""), PW : \(pwTextField.text ?? "")")
     }
-    @IBAction func SignUp(_ sender: UIButton) {
+    
+    @IBAction func signUp(_ sender: UIButton) {
         print("touch up inside - sign up")
+        
     }
     
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
